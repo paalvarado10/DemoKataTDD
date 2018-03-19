5304926a973e5ebc1d2c0d42831adef738e8f0b9
Pablo Alvarado <pa.alvarado10@uniandes.edu.co>
2018-03-18 19:46:51 -0500

    Rojo: Escribir prueba leer cadena con un numero

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -5,3 +5,6 @@ from Calculadora import Calculadora
 class CalculadoraTest(TestCase):
     def test_leer(self):
         self.assertEqual(Calculadora().leer(""),0,"Cadena Vacia")
+
+    def test_leer_un_numero(self):
+        self.assertEqual(Calculadora().leer("1"),1,"Un numero")
